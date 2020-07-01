c4aeaf466ca7ea7a6d13d6457f062a948f428faf
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-01 19:29:15 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_attractions.rb
@@ -0,0 +1,9 @@
+class CreateAttractions < ActiveRecord::Migration[5.0]
+ create_table :attractions do |t|
+    t.string :name
+    t.integer :tickets
+    t.integer :nausea_rating
+    t.integer :happiness_rating
+    t.integer :min_height
+  end
+end
